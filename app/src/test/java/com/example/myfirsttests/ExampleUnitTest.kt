8431542ec307c1785c2344d4a9e8a3473981441e
turkovaliudmila <ljudmila327@rambler.ru>
2021-10-24 16:55:24 +0300

    Выполнено ДЗ по уроку 1

--- app/src/test/java/com/example/myfirsttests/ExampleUnitTest.kt
+++ app/src/test/java/com/example/myfirsttests/ExampleUnitTest.kt
@@ -10,8 +10,49 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
+
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun emailValidator_CorrectEmailSimple_ReturnsTrue() {
+        assertTrue(EmailValidator.isValidEmail("name@email.com"))
+    }
+
+    @Test
+    fun emailValidator_CorrectEmailSubDomain_ReturnsTrue() {
+        assertTrue(EmailValidator.isValidEmail("name@email.co.uk"))
+    }
+
+    @Test
+    fun emailValidator_InvalidEmailNoTld_ReturnsFalse() {
+        assertFalse(EmailValidator.isValidEmail("name@email"))
+    }
+
+    @Test
+    fun emailValidator_InvalidEmailDoubleDot_ReturnsFalse() {
+        assertFalse(EmailValidator.isValidEmail("name@email..com"))
+    }
+
+    @Test
+    fun emailValidator_InvalidEmailNoUsername_ReturnsFalse() {
+        assertFalse(EmailValidator.isValidEmail("@email.com"))
+    }
+
+    @Test
+    fun emailValidator_EmptyString_ReturnsFalse() {
+        assertFalse(EmailValidator.isValidEmail(""))
+    }
+
+    @Test
+    fun emailValidator_NullEmail_ReturnsFalse() {
+        assertFalse(EmailValidator.isValidEmail(null))
+    }
+
+    @Test
+    fun emailValidator_CorrectEmail2_ReturnsTrue() {
+        assertEquals(EmailValidator.isValidEmail("name@email.ru"),true)
+    }
+
+    @Test
+    fun emailValidator_CorrectEmail3_ReturnsTrue() {
+        assertNotEquals(EmailValidator.isValidEmail("name@email"),true)
     }
 }
